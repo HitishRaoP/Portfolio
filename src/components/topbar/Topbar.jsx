68b5b2e61de16adab5b1c6e8bccda5cb4d2f736e
Hitish Rao P <hitishraop@gmail.com>
2024-01-23 20:52:27 +0530

    More CSS changes

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,15 +1,33 @@
 import "./Topbar.css";
+import EmailIcon from "@mui/icons-material/Email";
+import PersonIcon from "@mui/icons-material/Person";
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Topbar() {
   return (
     <div id="topbar">
       <div className="wrapper">
-        cen
         <div className="left">
-          <a href="#intro" className="logo">Hitish </a>
+          <a href="#intro" className="logo">
+            Hitish{" "}
+          </a>
+
+          <div className="items">
+            <PersonIcon />
+            <a className="icons" href="9019205231">
+              9019205231
+            </a>
+            <EmailIcon />
+            <a href="hitishraop@gmail.com" className="icons">
+              hitishraop@gmail.com
+            </a>
+          </div>
         </div>
+
         <div className="right">
-          right
+          <div className="hamburger">
+                <MenuIcon />
+          </div>
         </div>
       </div>
     </div>
